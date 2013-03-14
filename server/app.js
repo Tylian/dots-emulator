814edbe46752d1af10cb3a4b1a4a99ae8498c0bd
Tylian <immatyger@gmail.com>
2013-03-14 07:03:09 -0400

    Added version info to startup.

--- server/app.js
+++ server/app.js
@@ -2,9 +2,13 @@ var PolicyServer = require('./src/PolicyServer'),
     LobbyServer = require('./src/LobbyServer'),
     GameServer = require('./src/GameServer');
     
+var package = require('./package.json');
+
 // Set up the logger
 var winston = require('winston');
 winston.cli();
+
+winston.info('Begining startup of ' + package.name + ' v' + package.version);
 
 var policyServer = new PolicyServer();
 var lobbyServer = new LobbyServer();
